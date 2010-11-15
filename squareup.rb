f9ab2b50db6138bd2bff943a1f65266833cc8659
Rudi <rluzar@topmill.ca>
2010-11-15 13:37:37 -0500

    Refactored Squareup, it now has setup variables, which makes the time calculation more readible. Added more TODO items and some commets to the code

--- squareup.rb
+++ squareup.rb
@@ -6,26 +6,34 @@ class Squareup
   attr_accessor :plate_thickness, :plate_x, :plate_y
   
   def initialize (thick, x, y)
-    @plate_thickness = thick
-    @plate_x = x
-    @plate_y = y
-  end
+    @plate_thickness = thick  #plate thicknes in inches
+    @plate_x = x              #x size in inches
+    @plate_y = y              #y size in inches
+    @passes_per_inch = 20
+    @added_thickness = 0.350  #in inches extra to account for plunging past the part
+    @plunge_tool_feed = 50.0  #feed in inches per minute
+    @chamfer_tool_feed = 15.0 #feed in inches per minute
+    
+    # need error checking of input variables
+
+end
   
   def perimeter
     (plate_x + plate_y) * 2
   end
   
   def plunge_time
-    ( perimeter * 20 * (@plate_thickness + 0.350) ) / 50
+    # calculate number of seconds it takes to complete
+    ( perimeter * @passes_per_inch * (@plate_thickness + @added_thickness) ) / @plunge_tool_feed
   end
   
   def chamfer_time
     # (perimeter [in] / 15 [ipm] ) * 2 [plate sides]
-    ( perimeter / 15) * 2
+    ( perimeter / @chamfer_tool_feed) * 2
   end
   
   def setup_time
-    # 1h per plate
+    # 1h per plate (4 mini setups)
     60.0
   end
   
